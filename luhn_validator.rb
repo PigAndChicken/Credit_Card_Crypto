51832190fce7bdcd3d454485f82465b2f2937a0e
ShellyHsueh <hsueh821203@gmail.com>
2018-03-15 23:05:34 +0800

    A: luhn_validator & credit_card finished; test pass

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,9 +5,10 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
+    nums_reversed = nums_a.reverse
     # TODO: use the integers in nums_a to validate its last check digit
-    non_checked_nums = nums_a[0...(nums_a.length-1)]
-    idx = 0
+    non_checked_nums = nums_reversed[1..(nums_reversed.length)]
+    idx = 1
     doubled_num = non_checked_nums.map { |num|
       idx += 1
       idx.even? ? num*2 : num
@@ -16,8 +17,9 @@ module LuhnValidator
       digits = num.to_s.chars.map(&:to_i)
       digits.reduce(&:+)
     }.reduce(&:+)
-    sum_a = sum_digits.to_s.chars.map(&:to_i)
-    check_digit = 10 - sum_a[sum_a.length-1]
+    sum_digits_remainder = sum_digits % 10
+    sum_digits_remainder == 0 ?
+      check_digit = sum_digits_remainder : check_digit = (10 - sum_digits_remainder)
     nums_a[nums_a.length-1]==check_digit ? true : false
   end
 end
