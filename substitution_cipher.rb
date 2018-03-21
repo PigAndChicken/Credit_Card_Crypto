e06ab22aa8b3939aa8c4598bda87e921c097deff
ShellyHsueh <hsueh821203@gmail.com>
2018-03-21 22:15:54 +0800

    B-1: Caesar Cipher & Permutation Cipher finished

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,9 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      char_ords = document.chars.map(&:ord)
+      shifted_char_ords = char_ords.map { |ord| (ord + key) % 127 }
+      caesar_encrypt_string = shifted_char_ords.map(&:chr).join('')
     end
 
     # Decrypts String document using integer key
@@ -16,6 +19,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      caesar_encrypt_ords = document.chars.map(&:ord)
+      decrypt_ords = caesar_encrypt_ords.map { |num| (num - key) % 127 }
+      origin_string = decrypt_ords.map(&:chr).join('')
     end
   end
 
@@ -27,6 +33,12 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      ascii_arr = (0..127).to_a
+      permutation = ascii_arr.shuffle(random: Random.new(0))
+
+      char_ords = document.chars.map(&:ord)
+      encrypt_ords = char_ords.map { |char_ord| permutation[char_ord] }
+      permut_encrypt_string = encrypt_ords.map(&:chr).join('')
     end
 
     # Decrypts String document using integer key
@@ -36,6 +48,12 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      ascii_arr = (0..127).to_a
+      permutation = ascii_arr.shuffle(random: Random.new(0))
+
+      permut_encrypt_ords = document.chars.map(&:ord)
+      decrypt_ords = permut_encrypt_ords.map { |permut_ord| permutation.find_index(permut_ord) }
+      origin_string = decrypt_ords.map(&:chr).join('')
     end
   end
 end
