f63005a71ba538d04aee81518601ddbd270250b4
ShellyHsueh <hsueh821203@gmail.com>
2018-03-22 22:59:17 +0800

    fix

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -7,8 +7,9 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
-    cols_num = Math.sqrt(document.to_s.length).round
-    chars_matrix = doc_chars.each_slice(cols_num).to_a
+    doc_str = document.to_s
+    cols_num = Math.sqrt(doc_str.length).round
+    chars_matrix = doc_str.chars.each_slice(cols_num).to_a
 
     rows_shuffled_matrix = chars_matrix.shuffle(random: Random.new(key))
     double_shuffled_matrix = rows_shuffled_matrix.map { |row| row.shuffle(random: Random.new(key)) }
@@ -17,8 +18,9 @@ module DoubleTranspositionCipher
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
-    cols_num = Math.sqrt(ciphertext.to_s.chars.length).round
-    chars_matrix = cipher_chars.each_slice(cols_num).to_a
+    cipher_str = ciphertext.to_s
+    cols_num = Math.sqrt(cipher_str.length).round
+    chars_matrix = cipher_str.chars.each_slice(cols_num).to_a
 
     rows_restored_matrix = chars_matrix.shuffle(random: Random.new(key))
     double_shuffled_matrix = rows_restored_matrix.map { |row| row.shuffle(random: Random.new(key)) }
